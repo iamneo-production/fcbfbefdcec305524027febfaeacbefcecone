c30d95049bf8334bee53c423435908e151a825a6
Iamneo <techadmin@iamneo.ai>
2023-09-28 08:10:34 +0000

    Backup Commit

---
+++ springapp/src/model/Medicine.java
@@ -0,0 +1,61 @@
+package com.yourpackage.model;
+
+public class Medicine {
+    private int medicineId;
+        private String medicineName;
+            private float price;
+                private int quantity;
+                    private String description;
+
+                        public Medicine() {
+                            }
+
+                                public Medicine(int medicineId, String medicineName, float price, int quantity, String description) {
+                                        this.medicineId = medicineId;
+                                                this.medicineName = medicineName;
+                                                        this.price = price;
+                                                                this.quantity = quantity;
+                                                                        this.description = description;
+                                                                            }
+
+                                                                                public int getMedicineId() {
+                                                                                        return medicineId;
+                                                                                            }
+
+                                                                                                public void setMedicineId(int medicineId) {
+                                                                                                        this.medicineId = medicineId;
+                                                                                                            }
+
+                                                                                                                public String getMedicineName() {
+                                                                                                                        return medicineName;
+                                                                                                                            }
+
+                                                                                                                                public void setMedicineName(String medicineName) {
+                                                                                                                                        this.medicineName = medicineName;
+                                                                                                                                            }
+
+                                                                                                                                                public float getPrice() {
+                                                                                                                                                        return price;
+                                                                                                                                                            }
+
+                                                                                                                                                                public void setPrice(float price) {
+                                                                                                                                                                        this.price = price;
+                                                                                                                                                                            }
+
+                                                                                                                                                                                public int getQuantity() {
+                                                                                                                                                                                        return quantity;
+                                                                                                                                                                                            }
+
+                                                                                                                                                                                                public void setQuantity(int quantity) {
+                                                                                                                                                                                                        this.quantity = quantity;
+                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                public String getDescription() {
+                                                                                                                                                                                                                        return description;
+                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                                public void setDescription(String description) {
+                                                                                                                                                                                                                                        this.description = description;
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                            
