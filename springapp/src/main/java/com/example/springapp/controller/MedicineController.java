5f2bee0fc96d00680ccbd1a837ee07cefc6be15f
Iamneo <techadmin@iamneo.ai>
2023-10-09 10:49:57 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/springapp/controller/MedicineController.java
@@ -0,0 +1,37 @@
+package com.example.springapp.controller;
+
+import com.example.springapp.model.Medicine;
+import com.example.springapp.service.MedicineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicines")
+public class MedicineController {
+
+    @Autowired
+        private MedicineService medicineService;
+
+            @PostMapping("/")
+                public ResponseEntity<Boolean> addMedicine(@RequestBody Medicine medicine) {
+                        boolean added = medicineService.addMedicine(medicine);
+                                if (added) {
+                                            return new ResponseEntity<>(true, HttpStatus.CREATED);
+                                                    } else {
+                                                                return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+                                                                        }
+                                                                            }
+
+                                                                                @PutMapping("/{medicineId}")
+                                                                                    public ResponseEntity<Medicine> updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+                                                                                            Medicine updated = medicineService.updateMedicine(medicineId, updatedMedicine);
+                                                                                                    if (updated != null) {
+                                                                                                                return new ResponseEntity<>(updated, HttpStatus.OK);
+                                                                                                                        } else {
+                                                                                                                                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                                                                                                                                            }
+                                                                                                                                                }
+                                                                                                                                                }
+                                                                                                                                                
---
+++ springapp/src/main/java/com/example/springapp/service/MedicineService.java
@@ -0,0 +1,39 @@
+package com.example.springapp.service;
+
+import com.example.springapp.model.Medicine;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class MedicineService {
+
+    private Map<Integer, Medicine> medicineMap = new HashMap<>();
+        private int nextMedicineId = 1;
+
+            public boolean addMedicine(Medicine medicine) {
+                    if (medicineMap.containsKey(medicine.getMedicineId())) {
+                                return false; // Medicine with the same ID already exists
+                                        }
+                                                medicine.setMedicineId(nextMedicineId++);
+                                                        medicineMap.put(medicine.getMedicineId(), medicine);
+                                                                return true; // Medicine added successfully
+                                                                    }
+
+                                                                        public Medicine updateMedicine(int medicineId, Medicine updatedMedicine) {
+                                                                                if (medicineMap.containsKey(medicineId)) {
+                                                                                            Medicine existingMedicine = medicineMap.get(medicineId);
+                                                                                                        // Update fields of existingMedicine with values from updatedMedicine
+                                                                                                                    existingMedicine.setMedicineName(updatedMedicine.getMedicineName());
+                                                                                                                                existingMedicine.setPrice(updatedMedicine.getPrice());
+                                                                                                                                            existingMedicine.setQuantity(updatedMedicine.getQuantity());
+                                                                                                                                                        existingMedicine.setDescription(updatedMedicine.getDescription());
+                                                                                                                                                                    return existingMedicine;
+                                                                                                                                                                            }
+                                                                                                                                                                                    return null; // Medicine with the given ID not found
+                                                                                                                                                                                        }
+                                                                                                                                                                                        }
+                                                                                                                                                                                        
